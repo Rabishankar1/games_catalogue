fc49893ab4e9a8f58f7e166b2a55a1f60608dc12
Rabishankar1 <rabishankar.panigrahi14@gmail.com>
2025-07-13 07:28:23 +0530

    ignore-typescripts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://cdn.jackpot.bet/thumbnails/**")],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
